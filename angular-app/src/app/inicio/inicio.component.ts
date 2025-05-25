1a30e455998fccbd1f2020b480043478e5ec097d
Alexberemart <alexberemart@gmail.com>
2025-05-25 16:51:39 +0200

    inicio component was improved

--- angular-app/src/app/inicio/inicio.component.ts
+++ angular-app/src/app/inicio/inicio.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-inicio',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, MatCardModule, MatButtonModule],
   templateUrl: './inicio.component.html',
-  styleUrl: './inicio.component.css'
+  styleUrls: ['./inicio.component.css']
 })
 export class InicioComponent {
-
+  saldo = 12345.67;
+  ultimaActualizacion = new Date();
 }

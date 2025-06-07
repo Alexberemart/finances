998d557010cf9106671dbf2da3de665a9c4dd0a3
Alexberemart <alexberemart@gmail.com>
2025-06-07 14:41:28 +0200

    improving this feature

--- angular-app/src/app/infrastructure/services/cuenta.service.ts
+++ angular-app/src/app/infrastructure/services/cuenta.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
-import { BankAccount } from '../models/bank-account.model';
+import { BankAccount } from '../../models/bank-account.model'; // <-- FIXED
 
 @Injectable({
   providedIn: 'root'
--- angular-app/src/app/inicio/inicio.component.ts
+++ angular-app/src/app/inicio/inicio.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
-import { CuentaService } from '../services/cuenta.service';
+import { CuentaService } from '../infrastructure/services/cuenta.service'; // <-- FIXED
 import { BankAccount } from '../models/bank-account.model';
 
 @Component({

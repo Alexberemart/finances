9aa6a119b3495f30b00214adfebd510835a5a178
Alexberemart <alexberemart@gmail.com>
2025-06-07 12:49:41 +0200

    working on bbva import data feature

---
+++ angular-app/src/app/models/bbva-import-data.model.ts
@@ -0,0 +1,5 @@
+export interface BBVAImportData {
+  date: string;
+  description: string;
+  amount: number;
+}
---
+++ angular-app/src/app/models/csv-row.model.ts
@@ -0,0 +1,5 @@
+export interface CsvRow {
+  date: string;
+  description: string;
+  amount: number;
+}

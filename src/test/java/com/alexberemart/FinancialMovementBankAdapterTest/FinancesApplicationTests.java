f40619842ac4a53bdcf40da47985098b23cb52b1
Alexberemart <alexberemart@gmail.com>
2025-06-14 16:04:51 +0200

    cleaning

---
+++ src/test/java/com/alexberemart/FinancialMovementBankAdapterTest/FinancesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexberemart.FinancialMovementBankAdapterTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

9fde92ecff9c22ac4c86744140f936c2daa9dbc0
Alexberemart <alexberemart@gmail.com>
2025-06-14 20:33:40 +0200

    it works

--- angular-app/src/app/domain/test.ts
+++
@@ -1,8 +0,0 @@
-// src/app/domain/test-boundary.ts
-import { inject, Injectable } from '@angular/core';
-import { CuentaService } from '../infrastructure/services/cuenta.service';
-
-@Injectable({ providedIn: 'root' })
-export class ImportRestService {
-private something = inject(CuentaService);
-}
